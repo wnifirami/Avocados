457268063e70c8a9c8fe7ea1839980f650261095
wnifirami <rami.ounifi@esprit.tn>
2021-07-29 14:31:18 +0100

    initiate project and ressources

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Rami Ounifi on 29/7/2021.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
